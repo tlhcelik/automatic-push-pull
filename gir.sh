0fe736b7265857a84994343fb53781d3b5ebe83c
Talha Çelik <talhacelik81@hotmail.com>
2017-05-19 22:17:41 +0300

    updating
    adding new properties and lines

--- gir.sh
+++ gir.sh
@@ -3,40 +3,71 @@
 #__author__link__ = "@tlhcelik"
 #__author__nick__ = "MALC"
 clear
-echo '[*]Starting AGPoP ( Automatic GitHub Push or Pull ) Program'
+echo '[*]Starting AGPoP ( Automatic GitHub Push or Pull ) Script'
 
 username=" "
 password=" "
 
-
+# ekleme yapildi
+# status eklendi
+# elif ici exit
+# git remote add origin http
+# git pull origin master (brnch)
+# git push origin master (brnc)
+# kullanici pass istenecek
+# parametreli kullanim sekli yapil
 #----------------------------------------
-function create_new_project()
+create_new_project()
 {
-echo "new prorje"
-exit 
+	echo "[*]Give a project path ( /home/user/github_project )"
+	read PATH
+	
+	#git code
+	git status $PATH
+	
+	echo "[*]Red lines is don't added your locale .git repository"
+	echo "[*]Are you add ? [y/n]"
+	read -n 1 add
+	
+	if [[ $add = "y" ]]
+		then
+			git add * $PATH
+			git status $PATH
+			echo -e "\n[*]If all files is green, adding succesfuly"
+	elif [[ $add = "n" ]]
+		then
+			exit
+	fi
+	exit 
+}
+#----------------------------------------
+#----------------------------------------
+function existing_project()
+{
+	echo -e "\nexiststs prorje"
+	exit 
 }
 #----------------------------------------
 #----------------------------------------
 function get_project_type()
 {
-	echo '[*]Creating to new project ? [y/n]'
-	echo '[*]Give ansver Y or N'
+	echo -e '[*]Creating to new project ? [y/n]'
+	echo -e '\n[*]Give ansver Y or N'
 	read  -n 1 project_type
 
 if [[ $project_type = "y" ]]
 	then
-		echo '[*]Creating new project'
+		echo -e '\n[*]Creating new project'
 		create_new_project
 
 elif [[ $project_type = "n" ]]
 	then
-		echo '[*]Existing Project will insert'
+		echo -e '\n[*]Existing Project will insert'
+		existing_project
 
 else
 	get_project_type
 fi
-
-		
 }
 #----------------------------------------
 
